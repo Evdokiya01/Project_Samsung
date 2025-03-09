33f4e57fb0317aad1b9ebb148b73ddc98ca6c2d0
Evdokiya01 <oevlokia953@gmail.com>
2025-03-09 20:02:42 +0300

    другая разметка страницы аккаунта, небольшие изменения в странице регистраций, переход на другие страницы в аккаунте,пока не получается

--- app/src/main/java/com/example/project_samsung/MainActivity.java
+++ app/src/main/java/com/example/project_samsung/MainActivity.java
@@ -1,28 +1,24 @@
 package com.example.project_samsung;
 
 import android.annotation.SuppressLint;
+import android.media.Image;
 import android.os.Bundle;
-import android.text.Html;
-import android.text.method.LinkMovementMethod;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btn_log_in;
+    private Button btn_log_in, btn_up;
     private TextView textView;
-    private Button btn_up;
+    private ImageButton btn_account_forum, btn_account_market, btn_account_course, btn_account_account;
+    private ImageView image_view_account;
+
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -63,6 +59,44 @@ public class MainActivity extends AppCompatActivity {
 //                LogUp myObject = new LogUp();
 //                myObject.onCreate();
                 logUp();
+            }
+        });
+    }
+
+    @SuppressLint("MissingInflatedId")
+    private void account(){
+        setContentView(R.layout.account);
+        btn_account_market = findViewById(R.id.imageButtonAccountMarket);
+        btn_account_forum = findViewById(R.id.imageButtonAccountForum);
+        btn_account_course = findViewById(R.id.imageButtonAccountCourse);
+        btn_account_account = findViewById(R.id.imageButtonAccountAccount);
+        image_view_account = findViewById(R.id.imageViewAccount);
+
+        //image_view_account.setImageResource(R.drawable.forum);
+
+
+        btn_account_market.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.product);
+            }
+        });
+        btn_account_forum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.forum);
+            }
+        });
+        btn_account_course.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.courses_singular);
+            }
+        });
+        btn_account_account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                account();
             }
         });
     }
